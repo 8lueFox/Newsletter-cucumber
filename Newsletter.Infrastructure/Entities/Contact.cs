2700c3388cd61f54e7c46371693670513d64f5d4
8lueFox <kacper.jedrzejewski@int.pl>
2022-11-27 14:06:17 +0100

    Dodaj pliki projektów.

---
+++ Newsletter.Infrastructure/Entities/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace Newsletter.Infrastructure.Entities;
+
+public class Contact
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    public string Email { get; set; } = default!;
+}
